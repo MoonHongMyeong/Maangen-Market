ec586f10d6b2e3695fc76eee1b06fad742888385
MoonHongMyeong <moonhongmyeong@gmail.com>
2022-01-07 16:56:18 +0900

    [#2] 글로벌 예외처리 핸들러 작성

---
+++ src/main/java/me/moon/market/global/error/exception/EntityNotFoundException.java
@@ -0,0 +1,11 @@
+package me.moon.market.global.error.exception;
+
+public class EntityNotFoundException extends BusinessException{
+    public EntityNotFoundException(String message){
+        super(message, ErrorCode.ENTITY_NOT_FOUND);
+    }
+
+    public EntityNotFoundException(String message, ErrorCode code) {
+        super(message, code);
+    }
+}
