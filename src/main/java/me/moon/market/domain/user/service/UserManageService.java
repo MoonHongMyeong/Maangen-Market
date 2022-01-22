b188cfcce7efbe375e299958dcdcd7dfdbad837e
MoonHongMyeong <moonhongmyeong@gmail.com>
2022-01-22 16:16:30 +0900

    [#4] UserSignUpService -> UserManageService 변경

--- src/main/java/me/moon/market/domain/user/service/UserManageService.java
+++ src/main/java/me/moon/market/domain/user/service/UserManageService.java
@@ -2,11 +2,14 @@ package me.moon.market.domain.user.service;
 
 import lombok.RequiredArgsConstructor;
 import me.moon.market.domain.user.dao.UserRepository;
+import me.moon.market.domain.user.dto.LoginUserRequest;
 import me.moon.market.domain.user.dto.UserSaveRequest;
 import me.moon.market.domain.user.entity.User;
 import me.moon.market.domain.user.exception.EmailDuplicateException;
+import me.moon.market.domain.user.exception.LoginWrongException;
 import me.moon.market.domain.user.exception.NicknameDuplicateException;
 import me.moon.market.domain.user.exception.PhoneDuplicateException;
+import me.moon.market.global.dto.SessionUser;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,10 +17,12 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @Transactional
 @RequiredArgsConstructor
-public class UserSignUpService {
+public class UserManageService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final UserFindService findService;
+
 
     public User doSignUp(UserSaveRequest dto){
 
@@ -30,4 +35,11 @@ public class UserSignUpService {
         return userRepository.save(dto.toEntity());
     }
 
+    public SessionUser findUserByLoginRequest(LoginUserRequest dto) {
+        User user = findService.findByEmail(dto.getEmail());
+        if(!passwordEncoder.matches(dto.getPassword(), user.getPassword())){
+            throw new LoginWrongException("");
+        }
+        return new SessionUser(user);
+    }
 }
