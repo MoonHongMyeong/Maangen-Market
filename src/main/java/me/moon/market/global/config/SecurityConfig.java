e84c42c2d083b6ae61f64caf68c04e04f40be96a
MoonHongMyeong <moonhongmyeong@gmail.com>
2022-01-22 22:27:54 +0900

    Fix : UserService 의 PasswordEncoder 의존성을 못받는 문제 발생 -> BCryptPasswordEncoder를 빈으로 주입

---
+++ src/main/java/me/moon/market/global/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package me.moon.market.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/").permitAll();
+    }
+
+}
